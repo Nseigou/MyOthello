308becf774614ebc93a6c39764490fd177ca5380
Nseigou <nagano_1si@yahoo.co.jp>
2024-02-04 16:05:25 +0900

    🎉自己流オブジェクト指向完成

--- object_GameMaster.py
+++ object_GameMaster.py
@@ -2,68 +2,78 @@ from object_Board import Board
 from object_Player import Player
 
 class GameMaster:
-    def __init__(self,symbol,color):
-        player1 = Player(1, "白")
-        player2 = Player(-1, "黒")
-        board = Board()
-    
-        #ターンの動き全体
-    def _turn(self):
-        while True:
-            if self._input():  #正しくターンが終わったらupdate()へ
-                break
-            else:
-                continue       #正しい入力がされなかったらもう一度入力
+    def __init__(self):
+        self.player1 = Player(1, "白", "○")
+        self.player2 = Player(-1, "黒", "●")
+        self.board = Board(self.player1, self.player2)
+        self.player = self.player1
+        self.game_flag = False
 
 
-    #勝敗条件
-    def win(self):
-        if self.n_white == 0:     #白のコマが0個になったら  
-            print("----------------------\n")
-            self.draw()
-            print("\n黒の勝ちです")
-            self.game_flag = True
-        elif self.n_black == 0:    #黒のコマが0個になったら
-            print("----------------------\n")
-            self.draw()
-            print("\n白の勝ちです")
-            self.game_flag = True
-        elif self.n_blank == 0:   #全てのマスにコマが置かれたら
-            if self.n_white > -(self.n_black):    #白>黒
-                print("----------------------\n")
-                self.draw()
-                print("\n白の勝ちです")
-                self.game_flag = True
-            elif self.n_white < -(self.n_black):     #白<黒
-                print("----------------------\n")
-                self.draw()
-                print("\n黒の勝ちです")
-                self.game_flag = True
-            elif self.n_white == -(self.n_black):                             #白=黒
-                print("----------------------\n")
-                self.draw()
-                print("\n引き分けです")
-                self.game_flag = True
-
-            print("白：",self.n_white)
-            print("黒：",-self.n_black)
+    #ひっくり返したことによるコマ数の変化を相手に反映させる
+    def ajust(self):
+        if self.player == self.player1:
+            self.player2.num -= self.player.temp_change
+            self.player.temp_change = 0
+        else:
+            self.player1.num -= self.player.temp_change
+            self.player.temp_change = 0
 
     
     #アップデート関数(主にリセットとターン交代)
     def update(self):
-        self.player *= -1 #ターン交代
-        self.change_flag = False
-        print("-----------------")
-        self.draw()
-        if self.player == self.white:
-            print("\n白のターン○")
+        self.player.change_flag = False    
+        if self.player == self.player1:
+            self.player = self.player2
         else:
-            print("\n黒のターン●")
+            self.player = self.player1
+
+        print("-----------------")
+        self.board.draw()
+        print("\n{}のターン{}".format(self.player.color,self.player.mark))
+
+
+    #勝敗条件
+    def isWin(self):
+        if self.player1.num == 0 or self.player2.num == 0 or self.board.blank == 0:     #白のコマが0個になったら  
+            print("----------------------\n")
+            self.board.draw()
+            if self.player1.num > self.player2.num:
+                print("\n{}の勝ちです".format(self.player1.color))
+            elif self.player1.num < self.player2.num:
+                print("\n{}の勝ちです".format(self.player2.color))
+            else:
+                print("\n引き分けです")
+            self.game_flag = True
+        
+
+            print("白：",self.player1.num)
+            print("黒：",self.player2.num)
 
     
 
-    def gamestart():
+
+    def gamestart(self):
         
+        self.board.draw()
+        print("\n白：○　　黒：●")
+        print("-----------------\n{}のターン{}".format(self.player.color, self.player.mark))
+        while True:
+            while True:
+                if self.board.check(self.player):  #ひっくり返せるマスが存在している場合
+                    while True:
+                        if self.player.is_valid(self.board):  
+                            self.ajust()
+                            break
+                        else:
+                            continue
+                    break
+                else:
+                    print("コマを置けるマスがありません。あなたの番はパスされます。")
+                    self.update()  #相手の番にする
+            self.isWin()
+            if self.game_flag:   #勝敗が付いたら
+                break
+            self.update()
 
-
-
+        
--- object_main.py
+++ object_main.py
@@ -3,14 +3,15 @@ from object_GameMaster import GameMaster
 class Main:
     def __init__(self):
         gm = GameMaster()
+        gm.gamestart()
 
-    gm.gamestart()
 
 
 def object_main():
     main = Main()
 
-if __name__ == "__obnect_main__":
-    main()
+
+if __name__ == "__main__":
+    object_main()
 
 
